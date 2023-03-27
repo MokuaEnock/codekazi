7e60e2dacec29a256568b80983dcabbaa0049823
Enock <enock.mokua@student.moringaschool.com>
2023-03-27 10:48:15 +0300

    create ethods for jobs and categories methods for controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,19 @@
 class CategoriesController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def cat_params
+    params.permit(:name, :description)
+  end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,23 @@
 class JobsController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def index
+    render json: Job.all
+  end
+
+  private
+
+  def job_params 
+    paras.permit(:) 
+  end
 end
