7ba33a300658234b15c8579754f2e116c6cca446
Enock <enock.mokua@student.moringaschool.com>
2023-03-27 11:42:19 +0300

    update job model to include searchable content

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,23 @@
 class Job < ApplicationRecord
+  include PgSearch::Model
+  pd_search_scope :search_by_title_and_description,
+                  against: [:title, :description],
+                  using: {
+                    tsearch: { prefix: true },
+                  }
+  before_save :update_searchable_content
+
+  def update_searchable_content
+    self.searchable_content = [
+      title,
+      description,
+      company,
+      location,
+      category&.name,
+      salary_range,
+      employment_type,
+      years_of_experience,
+      education_level,
+    ].compact.join(" ")
+  end
 end
