3c4fc08d7fe14fcd195e24c0216cf42cbcbe3ba8
Enock <enock.mokua@student.moringaschool.com>
2023-03-27 12:14:36 +0300

    create seed data for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+categories = [
+  { name: "Software Development", description: "Creating software applications and systems" },
+  { name: "Data Science", description: "Analyzing and interpreting complex data" },
+  { name: "Web Design", description: "Creating visually appealing and functional websites" },
+  { name: "Marketing", description: "Promoting and selling products or services" },
+  { name: "Finance", description: "Managing financial transactions and investments" },
+  { name: "Human Resources", description: "Managing employee recruitment, selection, and training" },
+  { name: "Graphic Design", description: "Creating visual concepts and designs for various media" },
+  { name: "Project Management", description: "Organizing and overseeing projects from start to finish" },
+  { name: "Customer Service", description: "Providing support and assistance to customers" },
+  { name: "Sales", description: "Selling products or services to customers" },
+]
+
+categories.each do |category|
+  Category.create(category)
+end
