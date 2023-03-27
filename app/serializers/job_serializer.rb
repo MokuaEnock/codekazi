484d12d7291249b129bbe85615d829238673ada9
Enock <enock.mokua@student.moringaschool.com>
2023-03-27 15:16:32 +0300

    add attributes to modify the dates

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,13 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :description, :company, :location, :salary_range, :employment_type, :years_of_experience, :education_level, :posted_at, :expires_at, :is_active
 
   belongs_to :category
+
+  def posted_at
+    object.posted_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+  def expires_at
+    object.expires_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
 end
